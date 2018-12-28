1b50f5ff332374f0fe0e995ae8751ad67ed566c9
durgarao <pamarthidurgarao1@gmail.com>
2018-12-28 13:35:28 +0530

    login changes and home navigations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
